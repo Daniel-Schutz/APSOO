ac6a662f9fe2fe7edab99af56115bcf7d5b93e41
Daniel Schutz <109694403+Daniel-Schutz@users.noreply.github.com>
2023-05-18 15:06:55 -0400

    Create Conexao.java

---
+++ Conexao.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+
+public class Conexao {
+    private Connection conexao;
+    private String url;
+    private String usuario;
+    private String senha;
+    
+    public Conexao(String url, String usuario, String senha) {
+        this.url = url;
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+    
+    public void conectar() throws SQLException {
+        conexao = DriverManager.getConnection(url, usuario, senha);
+        System.out.println("Conexão estabelecida com sucesso!");
+    }
+    
+    public void desconectar() throws SQLException {
+        conexao.close();
+        System.out.println("Conexão encerrada com sucesso!");
+    }
+}
