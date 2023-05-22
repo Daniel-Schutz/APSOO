b6184f13bed620b2a225271a72690268d762aeb5
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-05-22 13:58:56 -0400

    cadastrar cliente - backend

--- model/Cliente.java
+++ model/Cliente.java
@@ -1,14 +1,20 @@
 package model;
+
+import DAO.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Cliente extends Pessoa {
+    PessoaDAO pessoaDAO;
     private String situacao;
 
     // Construtor da classe
-    public Cliente(String nome, String cpf, String email, String senha, String endereco, String situacao) {
+    public Cliente(PessoaDAO pessoaDAO, String nome, String cpf, String email, String senha, String endereco,
+            String situacao) {
         super(nome, cpf, email, senha, endereco);
         this.situacao = situacao;
+        this.pessoaDAO = pessoaDAO;
+
     }
 
     // Método getter para o atributo situacao
@@ -22,21 +28,48 @@ public class Cliente extends Pessoa {
     }
 
     // Método para cadastrar um cliente no banco de dados
-    public static void cadastrarCliente(String cpf, String nome, String email, String situacao, String senha, String endereco) {
-        // Implemente a lógica de cadastro do cliente no banco de dados aqui
+    public void cadastrarCliente() {
+        this.pessoaDAO.criarPessoa(this);
         System.out.println("Cliente cadastrado com sucesso!");
     }
 
-    // Método para verificar se um cliente existe no banco de dados pelo CPF
-    public static boolean existeCliente(String cpf) {
-        // Implemente a lógica de busca do cliente no banco de dados pelo CPF aqui
-        // Neste exemplo, utilizaremos um mapa simples para simular o banco de dados
-        Map<String, Cliente> bancoClientes = new HashMap<>();
-        // Supondo que o CPF seja a chave do mapa
-        return bancoClientes.containsKey(cpf);
+    public void atualizarCliente(){
+        pessoaDAO.atualizarPessoa(this);
+        System.out.println("Cliente atualizado com sucesso!");
+
     }
 
-    public void realizarAcao(){
+    // Método para verificar se um cliente existe no banco de dados pelo CPF
+    public static boolean existeCliente(PessoaDAO pessoaDAO, String cpf) {
+        // // Implemente a lógica de busca do cliente no banco de dados pelo CPF aqui
+        // // Neste exemplo, utilizaremos um mapa simples para simular o banco de dados
+        // Map<String, Cliente> bancoClientes = new HashMap<>();
+        // // Supondo que o CPF seja a chave do mapa
+        // return bancoClientes.containsKey(cpf);
+
+        return pessoaDAO.existePessoa(cpf);
+    }
+
+    public Cliente buscarCliente(String cpf) {
+
+        Cliente cliente;
+        cliente = this.pessoaDAO.buscarPessoa(cpf); // errado pq la no dao ta definido cpf como int ao inves de string
+        return cliente;
+    }
+
+    public Cliente[] buscarTodosCliente() {
+        Cliente[] objClientes;
+        // objClientes = this.pessoaDAO. CRIAR FUNÇAO DE BUSCAR TODOS OS CLIENTES NO DAO
+        return objClientes;
+    }
+
+    public void deletarCliente(String cpf) {
+        this.pessoaDAO.excluirPessoa(cpf);
+        System.out.println("Cliente deletado");
+    }
+
+
+    public void realizarAcao() {
 
     }
 }
