d23182e241d6da1a1f3cf08f8e7d1c1f6b3d99fd
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-05-23 17:05:28 -0400

    adicionando tipo ADMIN no construtor e removendo isAdministrador

--- model/Administrador.java
+++ model/Administrador.java
@@ -1,22 +1,11 @@
 package model;
 import java.util.Date;
 public class Administrador extends Funcionario {
-    private boolean isAdministrador;
 
     // Construtor da classe
-    public Administrador(String nome, String cpf, String email, String senha, String endereco, boolean isAdministrador) {
-        super(nome, cpf, email, senha, endereco);
-        this.isAdministrador = isAdministrador;
-    }
-
-    // Método getter para o atributo isAdministrador
-    public boolean isAdministrador() {
-        return isAdministrador;
-    }
-
-    // Método setter para o atributo isAdministrador
-    public void setAdministrador(boolean isAdministrador) {
-        this.isAdministrador = isAdministrador;
+    public Administrador(String nome, String cpf, String email, String senha, String endereco, double salario, Date dataContratacao, String telefone) {
+        super(nome, cpf, email, senha, endereco, salario, dataContratacao, telefone);
+        this.tipo = ADMIN;
     }
 
     // Método para cadastrar um funcionário
