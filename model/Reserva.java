3f8de9fc0bf000bbe43879c51999e2609a9e080f
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-05-23 18:25:37 -0400

    feat(Reserva.java): add try catch to buscarReserva

--- model/Reserva.java
+++ model/Reserva.java
@@ -28,10 +28,19 @@ public class Reserva {
         System.out.println("Registrada a reserva!!");
     }
 
-    public Reserva buscarReserva(int codigo){
-        Reserva reserva;
-        reserva = reserva.reservaDAO.buscarReserva(codigo);
-        return reserva;
+    public static Reserva buscarReserva(ReservaDAO reservaDAO,int codigo){
+        
+        //a função buscarReserva está declarando que pode lançar uma exceção do tipo SQLException
+        //por isso precisa de um try catch aqui
+        try {
+            Reserva reserva;
+            reserva = reservaDAO.buscarReserva(codigo);
+            return reserva;
+            
+        } catch (Exception e) {
+            return null;
+        }
+        
     }
 
     public buscarTodasReservas(){
