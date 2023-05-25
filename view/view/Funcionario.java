d7894c860fa7a4ea9f2a48fe737bc93e84bbff1c
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-05-24 22:52:12 -0400

    feat(Funcionario INterface): classe funcionario para uso TEMPORARIO

---
+++ view/view/Funcionario.java
@@ -0,0 +1,57 @@
+package view;
+public class Funcionario {
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private String endereco;
+
+    public Funcionario(String nome, String cpf, String email, String telefone, String endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+}
